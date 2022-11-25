9747bbc6437c501c6b028c6f4db180c1e70853df
Atanova Ulyana <ula122002@gmail.com>
2022-11-25 21:21:08 +0700

    yasss

--- 932003.atanova.ulyana.lab5/lab5.js
+++ 932003.atanova.ulyana.lab5/lab5.js
@@ -3,9 +3,10 @@ const news2 = document.getElementById('news2');
 const news3 = document.getElementById('news3');
 
 const open_news1 = document.getElementById('open_news1');
-const close_news1 = document.getElementsByClassName('content');
 const open_news2 = document.getElementById('open_news2');
 const open_news3 = document.getElementById('open_news3');
+
+// const first = document.getElementById('first');
 
 open_news1.addEventListener('click', 
 function(a){
@@ -13,15 +14,14 @@ function(a){
     news1.classList.add('active');}
 )
 
-document.addEventListener('keydown', function() {
-	if( key == 27 ){ 
-        code = String.fromCharCode(e.key);
-        console.log(code);
-		news1.classList.remove('active');
-	}
-});
-
-
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+    if (keyName==='Escape')
+    {
+        news1.classList.remove('active');
+    }
+})
 
 open_news2.addEventListener('click', 
 function(a){
@@ -29,8 +29,51 @@ function(a){
     news2.classList.add('active');}
 )
 
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+    if (keyName==='Escape')
+    {
+        news2.classList.remove('active');
+    }
+})
+
 open_news3.addEventListener('click', 
 function(a){
     a.preventDefault();
     news3.classList.add('active');}
 )
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+    if (keyName==='Escape')
+    {
+        news3.classList.remove('active');
+    }
+})
+
+first_news.onmouseover = function (){
+    first_news.style.background = "red";
+};
+
+first_news.onmouseleave = function (){
+    first_news.style.background = "rgb(164, 251, 251)";
+};
+
+second_news.onmouseover = function (){
+    second_news.style.background = "red";
+};
+
+second_news.onmouseleave = function (){
+    second_news.style.background = "rgb(164, 251, 251)";
+};
+
+third_news.onmouseover = function (){
+    third_news.style.background = "red";
+};
+
+third_news.onmouseleave = function (){
+    third_news.style.background = "rgb(164, 251, 251)";
+};
+
